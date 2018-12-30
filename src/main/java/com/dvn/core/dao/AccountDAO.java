e7bd8592ab5828c89ddd3d2506422a5e03daccf8
Anderson Soriano <19699525+andersori@users.noreply.github.com>
2018-12-30 16:32:45 -0300

    Dao de Account corrigido. Algumas funções adicionadas.

--- src/main/java/com/dvn/core/dao/AccountDAO.java
+++ src/main/java/com/dvn/core/dao/AccountDAO.java
@@ -9,7 +9,8 @@ public interface AccountDAO {
 	public List<Account> getAll();
 	public boolean set(Account ac);
 	public Account get(Long id);
+	public Account get(String username);
 	public boolean update(Account ac);
-	public boolean remove(Account ac);
+	public boolean remove(Long id);
 	
 }
--- src/main/java/com/dvn/core/dao/AccountDAOPostgres.java
+++ src/main/java/com/dvn/core/dao/AccountDAOPostgres.java
@@ -14,9 +14,18 @@ import com.dvn.core.model.Account;
 import com.dvn.core.util.Conversion;
 
 public class AccountDAOPostgres implements AccountDAO{
+	
+	private final static String TABLENAME = "core.account";
+	
+	private final static String COLUMNUSERID = "user_id";
+	private final static String COLUMNNAME = "name";
+	private final static String COLUMNUSERNAME = "username";
+	private final static String COLUMNPASSWORD = "password";
+	private final static String COLUMNCREATEDON = "created_on";
+	private final static String COLUMNLASTLOGIN = "last_login";
 
 	public List<Account> getAll() {
-		String sql = "SELECT * FROM core.account;";
+		String sql = String.format("SELECT * FROM %s;", TABLENAME);
 		List<Account> list = new ArrayList<Account>();
 		
 		Connection conn = ConnectionDB.getConn();
@@ -28,12 +37,12 @@ public class AccountDAOPostgres implements AccountDAO{
 			while(rs.next()) {
 				Account ac = new Account();
 				
-				ac.setId(rs.getLong("user_id"));
-				ac.setName(rs.getString("name"));
-				ac.setUsername(rs.getString("username"));
-				ac.setPassword(rs.getString("password"));
-				ac.setCreatedOn(Conversion.timestampToLocalDate(rs.getTimestamp("created_on")));
-				ac.setLastLogin(Conversion.timestampToLocalDate(rs.getTimestamp("last_login")));
+				ac.setId(rs.getLong(COLUMNUSERID));
+				ac.setName(rs.getString(COLUMNNAME));
+				ac.setUsername(rs.getString(COLUMNUSERNAME));
+				ac.setPassword(rs.getString(COLUMNPASSWORD));
+				ac.setCreatedOn(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNCREATEDON)));
+				ac.setLastLogin(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNLASTLOGIN)));
 				
 				list.add(ac);
 			}
@@ -49,7 +58,7 @@ public class AccountDAOPostgres implements AccountDAO{
 	}
 	
 	public boolean set(Account ac) {
-		String sql = "INSERT INTO core.account(username, name, password, created_on) VALUES (?, ?, ?, ?);";
+		String sql = String.format("INSERT INTO %s(%s, %s, %s, %s) VALUES (?, ?, ?, ?);", TABLENAME, COLUMNUSERNAME, COLUMNNAME, COLUMNPASSWORD, COLUMNCREATEDON);
 		Connection conn = ConnectionDB.getConn();
 		
 		try {
@@ -69,24 +78,57 @@ public class AccountDAOPostgres implements AccountDAO{
 		
 		return false;
 	}
+	
 	public Account get(Long id) {
-		String sql = "";
+		String sql = String.format("SELECT * FROM %s WHERE %s = ?;", TABLENAME, COLUMNUSERID);
 		Connection conn = ConnectionDB.getConn();
 		Account ac = null;
 		
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setLong(1, id);
 			ResultSet rs = ps.executeQuery();
 			
 			if(rs.next()) {
 				ac = new Account();
 				
-				ac.setId(rs.getLong("user_id"));
-				ac.setName(rs.getString("name"));
-				ac.setUsername(rs.getString("username"));
-				ac.setPassword(rs.getString("password"));
-				ac.setCreatedOn(Conversion.timestampToLocalDate(rs.getTimestamp("created_on")));
-				ac.setLastLogin(Conversion.timestampToLocalDate(rs.getTimestamp("last_login")));
+				ac.setId(rs.getLong(COLUMNUSERID));
+				ac.setName(rs.getString(COLUMNNAME));
+				ac.setUsername(rs.getString(COLUMNUSERNAME));
+				ac.setPassword(rs.getString(COLUMNPASSWORD));
+				ac.setCreatedOn(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNCREATEDON)));
+				ac.setLastLogin(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNLASTLOGIN)));
+			}
+			
+			rs.close();
+			ps.close();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return ac;
+	}
+	
+	public Account get(String username) {
+		String sql = String.format("SELECT * FROM %s WHERE %s = ?;", TABLENAME, COLUMNUSERNAME);
+		Connection conn = ConnectionDB.getConn();
+		Account ac = null;
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				ac = new Account();
+				
+				ac.setId(rs.getLong(COLUMNUSERID));
+				ac.setName(rs.getString(COLUMNNAME));
+				ac.setUsername(rs.getString(COLUMNUSERNAME));
+				ac.setPassword(rs.getString(COLUMNPASSWORD));
+				ac.setCreatedOn(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNCREATEDON)));
+				ac.setLastLogin(Conversion.timestampToLocalDate(rs.getTimestamp(COLUMNLASTLOGIN)));
 			}
 			
 			rs.close();
@@ -100,12 +142,44 @@ public class AccountDAOPostgres implements AccountDAO{
 	}
 
 	public boolean update(Account ac) {
-		// TODO Auto-generated method stub
+		String sql = String.format("UPDATE %s SET %s=?, %s=?, %s=?, %s=? WHERE %s=?;", TABLENAME, COLUMNUSERNAME, COLUMNNAME, COLUMNPASSWORD, COLUMNLASTLOGIN, COLUMNUSERID);
+		Connection conn = ConnectionDB.getConn();
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, ac.getUsername());
+			ps.setString(2, ac.getName());
+			ps.setString(3, ac.getPassword());
+			ps.setTimestamp(4, Conversion.localDateToTimestamp(ac.getLastLogin()));
+			ps.setLong(5, ac.getId());
+			
+			ps.executeUpdate();
+			ps.close();
+			
+			return true;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
 		return false;
 	}
 
-	public boolean remove(Account ac) {
-		// TODO Auto-generated method stub
+	public boolean remove(Long id) {
+		String sql = String.format("DELETE FROM %s WHERE %s=?;", TABLENAME, COLUMNUSERID);
+		Connection conn = ConnectionDB.getConn();
+		
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setLong(1, id);
+			
+			ps.executeUpdate();
+			ps.close();
+			
+			return true;
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
